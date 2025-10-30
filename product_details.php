e831f89772dbaccc3f0635d03319d5fcd5641ed9
Hloni <hloniletsaba@gmail.com>
2025-10-30 09:53:29 +0200

    Product detail

--- product_details.php
+++ product_details.php
@@ -34,6 +34,55 @@ if (isset($_GET['rim_id']) && is_numeric($_GET['rim_id'])){
     <title>Document</title>
 </head>
 <body>
-    
+    <img src="<?= $product['image_url'] ?>" alt="<?= $product['rim_name'] ?>"> 
+    <br>
+    <h3><?= $product['rim_name'] ?></h3>
+    <p><strong> <?= $product['model'] ?> </strong> </p>
+    <br>
+    <p class="price">R<?= $product['price'] ?></p>
+    <h4>Specifications:</h4>
+    <br>
+    <p><strong>Size:</strong> <?= $product['size_inch'] ?> inch </p>
+    <p><strong>Color:</strong> <?= $product['color'] ?> </p>
+    <br>
+    <p><strong>Bolt Pattern:</strong> <?= $product['bolt_pattern'] ?> </p>
+    <p><strong>Offset:</strong> <?= $product['offset'] ?> </p>
+    <br>
+    <p><strong>Center Bore:</strong> <?= $product['center_bore'] ?> </p>
+    <br>
+    <div class="quantity-container">
+    <label for="quantity">Quantity:</label>
+    <div class="quantity-box">
+        <button type="button" class="qty-btn" id="decrease">−</button>
+        <input type="text" id="quantity" value="1" readonly>
+        <button type="button" class="qty-btn" id="increase">+</button>
+    </div>
+</div>
+
+<button class="add-to-cart">ADD TO CART</button>
+
+
+
+
+
+
+
+<script>
+const decreaseBtn = document.getElementById('decrease');
+const increaseBtn = document.getElementById('increase');
+const quantityInput = document.getElementById('quantity');
+
+decreaseBtn.addEventListener('click', () => {
+    let value = parseInt(quantityInput.value);
+    if (value > 1) {
+        quantityInput.value = value - 1;
+    }
+});
+
+increaseBtn.addEventListener('click', () => {
+    let value = parseInt(quantityInput.value);
+    quantityInput.value = value + 1;
+});
+</script>
 </body>
 </html>
