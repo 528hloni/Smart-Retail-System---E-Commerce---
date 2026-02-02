e336f6ff539d2feb657224ed22603cbcff3fa2c6
Hloni <hloniletsaba@gmail.com>
2026-02-02 23:33:20 +0200

    Index navigation bar

--- index.php
+++ index.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+     <title>Landing Page</title>
+    <link rel="stylesheet" href="css/index.css">
+
 </head>
 <body>
-    <h1> Welcome to the landing page </h1>
+    <nav class="navbar">
+    <div class="nav-container">
+        <div class="nav-logo">
+            <a href="customer_dashboard.php>">Wheels of Fortune</a>
+        </div>
+        <ul class="nav-links">
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </div>
+</nav>
 </body>
 </html>
